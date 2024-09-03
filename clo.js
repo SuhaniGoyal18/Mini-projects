558b58b409dbad3dea90ec7e330d7da66c9488d3
SuhaniGoyal18 <suhanigoyal1814@gmail.com>
2024-09-03 19:29:15 +0530

    Add files via upload

---
+++ clo.js
@@ -0,0 +1,23 @@
+let hr=document.getElementById('hour');
+let min=document.getElementById('min');
+let sec=document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+
+    //getting hour,mins,secs from dadte
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hR = 30*hh + mm/2;
+    let mR = 6*mm;
+    let sR = 6*ss;
+
+    hr.style.transform = `rotate(${hR}deg)`;
+    min.style.transform = `rotate(${mR}deg)`;
+    sec.style.transform = `rotate(${sR}deg)`;
+}
+
+setInterval(displayTime, 1000);
